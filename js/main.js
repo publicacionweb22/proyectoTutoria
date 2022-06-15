67d7e4780b091aeb13adac6bd71f20434a93b302
publicacionweb22 <publicacionweb22@gmail.com>
2022-06-15 18:52:56 +0200

    several changes

--- js/main.js
+++ js/main.js
@@ -12,3 +12,17 @@ window.addEventListener('scroll', function () {
         header.classList.remove('transparent');
     }
 })
+
+// Bloque para abrir y cerrar el menú lateral
+
+var aside = document.querySelector('aside');
+var burger = document.querySelector('.burger');
+
+burger.addEventListener('click', function () {
+    aside.classList.toggle('open');
+    burger.classList.toggle('open');
+})
+
+// Bloque para añadir el texto del copyright
+
+document.querySelector('.copyright').innerHTML = '&copy; ' + new Date().getFullYear() + ' Real State S.A.';
