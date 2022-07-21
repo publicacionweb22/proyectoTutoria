8388b76e509d56f9f52ab9e6be45dde90666bc7e
publicacionweb22 <publicacionweb22@gmail.com>
2022-07-21 14:37:46 +0200

    jquery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+// Bloque para abrir y cerrar el menú lateral
+
+// var aside = document.querySelector('aside');
+// var burger = document.querySelector('.burger');
+
+// burger.addEventListener('click', function () {
+//     aside.classList.toggle('open');
+//     burger.classList.toggle('open');
+// })
+
+// Opción del mismo código pero ejecutado con JQuery
+// JQuery se usa dentro de código JavaScript
+
+$(document).ready(function () { // Asegurarnos que la página está cargada
+    // Usamos los métodos JQuery
+    $('.burger').click(function () {
+        $('aside').toggleClass('open');
+        $('.burger').toggleClass('open');
+    })
+})
+
 // Bloque para cambiar la clase CSS de la cabecera
 
 var primeraSeccion = document.getElementById('first');
@@ -13,15 +34,6 @@ window.addEventListener('scroll', function () {
     }
 })
 
-// Bloque para abrir y cerrar el menú lateral
-
-var aside = document.querySelector('aside');
-var burger = document.querySelector('.burger');
-
-burger.addEventListener('click', function () {
-    aside.classList.toggle('open');
-    burger.classList.toggle('open');
-})
 
 // Bloque para añadir el texto del copyright
 
